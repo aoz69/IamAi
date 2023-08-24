8fb4ef9b96e1d719e6036dd09b725dab257f3752
aoz69 <aozpokharel@gmail.com>
2023-08-24 21:31:45 +0545

    fetchedProducts

--- client/src/component/product.js
+++ client/src/component/product.js
@@ -3,6 +3,7 @@ import PieChart from 'react-native-pie-chart';
 import React, { Component } from 'react';
 import useFetchData  from './fetchTest'
 import vars from '../public/vars';
+import ProductTable from './productsTable'
 
 const PieComponent = () => {
   const ip = vars();
@@ -10,67 +11,20 @@ const PieComponent = () => {
   const products = useFetchData(ip +'fetchProducts');
 //   const soldStock = useFetchData(ip +'soldCount');
     console.log(products)
+    return (
+      <View>
+        {products.products && products.products.map((product) => (
+          <View key={product._id}>
+            <Text>{product.name}</Text>
+            <Text>{product.price}</Text>
+            <Text>{product.stock_Count}</Text>
+            <Text>{product.status}</Text>
 
-    const renderProduct = ({ products }) => (
-        <View style={styles.productRow}>
-          <Text style={styles.productName}>{products.name}</Text>
-          <Text style={styles.productPrice}>{products.price}</Text>
-          <Text style={styles.productStock}>{products.stock_Count}</Text>
-          <Text style={styles.productStatus}>{products.status}</Text>
-        </View>
-      );
-    
-      return (
-        <View style={styles.container}>
-          <View style={styles.headerRow}>
-            <Text style={styles.headerText}>Name</Text>
-            <Text style={styles.headerText}>Price</Text>
-            <Text style={styles.headerText}>Stock Count</Text>
-            <Text style={styles.headerText}>Status</Text>
           </View>
-          <FlatList
-            data={products}
-            renderItem={renderProduct}
-            keyExtractor={(products) => products._id}
-          />
-        </View>
-      );
+        ))}
+      </View>
+    );
+    
 };
 
 export default PieComponent;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row', // Layout children in a row
-    margin: '10%',
-    marginRight: 0, // Remove right margin to keep the grids side by side
-  },
-  pieContainer: {
-    flex: 1, // Occupy 1/2 of the available space for the pie chart
-  },
-  colorDetailsContainer: {
-    flex: 1, // Occupy 1/2 of the available space for the color details
-    marginLeft: 10, // Add some spacing between the pie chart and color details
-    marginTop: 30,
-  },
-  colorDetailItem: {
-    flexDirection: 'row', // Layout children in a row
-    marginLeft: 30, // Add spacing between color square and color name
-    marginBottom: 10,
-  },
-  colorSquare: {
-    width: 20,
-    height: 20,
-    marginRight: 10, // Add spacing between color square and color name
-  },
-  colorText: {
-    fontSize: 15,
-    color: '#27374D',
-  },
-  valueText: {
-    fontSize: 15,
-    color: '#27374D',
-    marginLeft: 'auto', // Push the value text to the right
-  },
-});
